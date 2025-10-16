8e662a21295ee0c1422f258c60e66034aeaa1936
JoJoJoDoDoDo <itsoref2028@kehillahstudent.org>
2025-10-16 09:15:31 -0700

    KEo

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import turtle
 
 turtle.speed(0)
 
-size = 33 # controls the size of the petals. All values
-count = 30 # controls the count of the petals. All values
+size = 15 # controls the size of the petals. All values
+count = 10 # controls the count of the petals. All values
 
-turtle.color("purple") #controls the color of the petals
-turtle.pensize(3) # controls the thickness and width of each of the petals
+
+
+
 
 def draw_petal():
     for i in range(20): # first petal
@@ -18,10 +19,49 @@ def draw_petal():
         turtle.right(3)
     turtle.right(120)
 
+def draw_leaf():
+        for i in range(20): # first leaf
+            turtle.forward(size / 3)
+            turtle.right(3)
+        turtle.right(120)
+        for i in range(20): # second leaf
+            turtle.forward(size / 3)
+            turtle.right(3)
+        turtle.right(120)
 
+def draw_stem():
+    turtle.penup()
+    turtle.setposition(0,100)
+    turtle.pendown()
+    turtle.pensize(2)
+    turtle.color("green")
+    turtle.setheading(270)
+    turtle.forward(12*size)
+    turtle.setheading(180)
+    draw_leaf()
+    for i in range (2):
+        turtle.setheading(270)
+        turtle.forward(50)
+        turtle.setheading(70-20*i)
+        draw_leaf()
+        turtle.setheading(270)
+        turtle.forward(50)
+        turtle.setheading(180+20*i)
+        draw_leaf()
+    turtle.setheading(270)
+    turtle.forward(200)
+    turtle.color("purple") #controls the color of the petals
+    turtle.pensize(3) # controls the thickness and width of each of the petals
+    turtle.penup()
+    turtle.setposition(0,100)
+    turtle.setheading(0)
+    turtle.pendown()
+
+draw_stem()
 for i in range(count): # draws the petals based of the count
     draw_petal()
     turtle.right(360 / count)
+
 
 # def draw_stem(): # draws the stem
    # turtle.setheading(270)
--- readme.md
+++ readme.md
@@ -3,16 +3,24 @@
 
 1. Roughly, what will your drawing look like
 
+Should look like a flower
 
 2. What are two variables you could use so that when you change each variable, the drawing changes?
+
+Count will control the amount of petals on the flower, and size will control the size of the petals.
 
 
 3. Where is there repetition in your drawing?
 
+in the making of the petals of the flower
 
 4. What are the main parts of your drawing? Are there clear sections or objects?
 
+the main parts of my drawing will be the beautiful petals of the flower. They will be the most visible objects in the drawing
 
 5. How could you use a loop variable of a for loop? Is there somewhere in your drawing where something is repeated, but it's slightly different each time? Maybe the same object is repeated in different places, or maybe a similar object is repeated but with different sizes?
+
+I could use it for creating the petals to be equal and quick. Yes, the petals will be repeated and slightly different every time. It's the same object, but potentially different colors, sizes, or general shape.
+
 
 
